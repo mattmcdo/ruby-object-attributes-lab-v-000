79c8b61b727dbcd965de52563e03fd63ce911d88
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-06 18:51:20 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,19 @@
 class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+  def job=(person_job)
+    @job = person_job
+  end
+  def job
+    @job
 end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+beyonce.job = "Singer"
